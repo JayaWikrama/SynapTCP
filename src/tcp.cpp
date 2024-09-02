6c861beebfd231c42fc92a42b1d400f8ca0ff5e8
JayaWikrama <jayawikrama89@gmail.com>
2024-09-02 23:18:30 +0700

    development/progress: add IP Address validator method.

--- src/tcp.cpp
+++ src/tcp.cpp
@@ -41,3 +41,86 @@ static void __TCP(TCP *obj){
 TCP::TCP(){
   __TCP(this);
 }
+
+/**
+ * @brief Check is IP Address valid or not.
+ *
+ * This method is responsible for validating whether the IP Address is valid or not.
+ *
+ * @param[in] address The IP Address (with a size of 4 bytes).
+ * @return `true` when the IP Address is valid
+ * @return `false` when the IP Address is invalid
+ */
+bool TCP::isValidIPAddress(const unsigned char *address){
+  if (address[0] == 255 &&
+      address[1] == 255 &&
+      address[2] == 255 &&
+      address[3] == 255
+  ){
+    return false;
+  }
+  return true;
+}
+
+/**
+ * @brief Overloaded method for `isValidIPAddress` to check is IP Address valid or not.
+ *
+ * This method is responsible for validating whether the IP Address is valid or not.
+ *
+ * @param[in] address The IP Address (with a size of 4 bytes).
+ * @return `true` when the IP Address is valid
+ * @return `false` when the IP Address is invalid
+ */
+bool TCP::isValidIPAddress(const std::vector <unsigned char> address){
+  if (address.size != 4) return false;
+  return this->isValidIPAddress(address.data());
+}
+
+/**
+ * @brief Overloaded method for `isValidIPAddress` to check is IP Address valid or not.
+ *
+ * This method is responsible for validating whether the IP Address is valid or not.
+ *
+ * @param[in] address The IP Address in string form.
+ * @return `true` when the IP Address is valid
+ * @return `false` when the IP Address is invalid
+ */
+bool TCP::isValidIPAddress(const char *address){
+  if (strlen(address) > 15){
+    return false;
+  }
+  unsigned short cnt = 0;
+  unsigned short value = 0;
+  do {
+    if (*address == '.'){
+      value = 0;
+      cnt++;
+    }
+    else if (*address >= 0x30 && *address <= 0x39){
+      value *= 10;
+      value += static_cast<unsigned short>(*address - 0x30);
+      if (value >= 255){
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+    address++;
+  } while (*address != 0x00);
+  if (cnt > 3) return false;
+  return true;
+}
+
+/**
+ * @brief Overloaded method for `isValidIPAddress` to check is IP Address valid or not.
+ *
+ * This method is responsible for validating whether the IP Address is valid or not.
+ *
+ * @param[in] address The IP Address in string form.
+ * @return `true` when the IP Address is valid
+ * @return `false` when the IP Address is invalid
+ */
+bool TCP::isValidIPAddress(const std::string address){
+  return this->isValidIPAddress(address.c_str());
+}
