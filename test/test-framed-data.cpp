a6ca45b2e37dc22ed3f6843c0f06f9341c5d59e5
Jaya Wikrama <jayawikrama89@gmail.com>
2024-10-06 13:18:52 +0700

    development/progress: add test for Framed Data Reception Method - 5.

--- test/test-framed-data.cpp
+++ test/test-framed-data.cpp
@@ -471,3 +471,61 @@ TEST_F(TCPFramedDataTest, ReceptionTest_withPrefixAndSuffix_2) {
     ASSERT_EQ(tmp.size(), 60);
     ASSERT_EQ(memcmp(tmp.data(), (unsigned char *) "qwertyuiopplkjhgfdsaZxcvbh76redcvbnm,mvdswertyuioiuhgfcxvbnm", 60), 0);
 }
+
+TEST_F(TCPFramedDataTest, ReceptionTest_withPrefixAndSuffix_3) {
+    unsigned char buffer[16];
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    int i = 0;
+    std::vector <unsigned char> tmp;
+    client.setPort(4431);
+    client.setTimeout(250);
+    client.setKeepAliveMs(1000);
+    DataFrame startBytes(DataFrame::FRAME_TYPE_START_BYTES, "1234");
+    DataFrame cmdBytes(DataFrame::FRAME_TYPE_COMMAND, 1);
+    cmdBytes.setPostExecuteFunction((const void *) &setupLengthByCommand, nullptr);
+    DataFrame dataBytes(DataFrame::FRAME_TYPE_DATA);
+    DataFrame stopBytes(DataFrame::FRAME_TYPE_STOP_BYTES, "90-=");
+    client = startBytes + cmdBytes + dataBytes + stopBytes;
+    ASSERT_EQ(client.getFormat()->getDataFrameFormat(),
+              "FRAME_TYPE_START_BYTES[size:4]:<<31323334>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_COMMAND[size:1]:<<>><<exeFunc:0>><<postFunc:" + std::to_string((unsigned long) &setupLengthByCommand) + ">>\n"
+              "FRAME_TYPE_DATA[size:0]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_STOP_BYTES[size:4]:<<39302D3D>><<exeFunc:0>><<postFunc:0>>\n");
+    ASSERT_EQ(client.init(), 0);
+    ASSERT_EQ(client.sendData("qwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnmqwertyuiop[1234567890-=zxcvbnm"), 0);
+    for (i = 0; i < 4; i++){
+        gettimeofday(&tvStart, NULL);
+        ASSERT_EQ(client.receiveFramedData(), 0);
+        gettimeofday(&tvEnd, NULL);
+        diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+        ASSERT_EQ(diffTime <= 75, true);
+        ASSERT_EQ(client.getDataSize(), 12);
+        ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 12);
+        ASSERT_EQ(memcmp(buffer, (const unsigned char *) "1234567890-=", 12), 0);
+        ASSERT_EQ(client.getBuffer(tmp), 12);
+        ASSERT_EQ(tmp.size(), 12);
+        ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "1234567890-=", 12), 0);
+    }
+    ASSERT_EQ(client.getRemainingDataSize(), 7);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 7);
+    ASSERT_EQ(memcmp(buffer, (unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 7);
+    ASSERT_EQ(tmp.size(), 7);
+    ASSERT_EQ(memcmp(tmp.data(), (unsigned char *) "zxcvbnm", 7), 0);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.receiveFramedData(), 2);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 240 && diffTime <= 280, true);
+    ASSERT_EQ(client.getDataSize(), 7);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 7);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 7);
+    ASSERT_EQ(tmp.size(), 7);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "zxcvbnm", 7), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 0);
+    ASSERT_EQ(tmp.size(), 0);
+}
