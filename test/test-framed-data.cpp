2bd8479e326e51a6123060f96a5d801758f6d977
Jaya Wikrama <jayawikrama89@gmail.com>
2024-10-06 13:19:47 +0700

    development/progress: add test for Framed Data Reception Method - 6.

--- test/test-framed-data.cpp
+++ test/test-framed-data.cpp
@@ -529,3 +529,58 @@ TEST_F(TCPFramedDataTest, ReceptionTest_withPrefixAndSuffix_3) {
     ASSERT_EQ(client.getRemainingBuffer(tmp), 0);
     ASSERT_EQ(tmp.size(), 0);
 }
+
+TEST_F(TCPFramedDataTest, ReceptionTest_withPrefixAndSuffix_4) {
+    unsigned char buffer[64];
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    std::vector <unsigned char> tmp;
+    client.setPort(4431);
+    client.setTimeout(250);
+    client.setKeepAliveMs(1000);
+    DataFrame startBytes(DataFrame::FRAME_TYPE_START_BYTES, 1, (const unsigned char *) "\x02");
+    DataFrame lengthBytes(DataFrame::FRAME_TYPE_CONTENT_LENGTH, 1);
+    lengthBytes.setPostExecuteFunction((const void *) &setupLengthByCommand2, (void *) &client);
+    DataFrame canBytes(DataFrame::FRAME_TYPE_DATA, 1);
+    DataFrame cmdBytes(DataFrame::FRAME_TYPE_COMMAND, 1);
+    DataFrame idBytes(DataFrame::FRAME_TYPE_DATA, 1);
+    DataFrame dataBytes(DataFrame::FRAME_TYPE_DATA);
+    DataFrame crcBytes(DataFrame::FRAME_TYPE_VALIDATOR, 2);
+    DataFrame stopBytes(DataFrame::FRAME_TYPE_STOP_BYTES, 1, (const unsigned char *) "\x03");
+    client = startBytes + lengthBytes + canBytes + cmdBytes + idBytes + dataBytes + crcBytes + stopBytes;
+    ASSERT_EQ(client.getFormat()->getDataFrameFormat(),
+              "FRAME_TYPE_START_BYTES[size:1]:<<02>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_CONTENT_LENGTH[size:1]:<<>><<exeFunc:0>><<postFunc:" + std::to_string((unsigned long) &setupLengthByCommand2) + ">>\n"
+              "FRAME_TYPE_DATA[size:1]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_COMMAND[size:1]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_DATA[size:1]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_DATA[size:0]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_VALIDATOR[size:2]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_STOP_BYTES[size:1]:<<03>><<exeFunc:0>><<postFunc:0>>\n");
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.init(), 0);
+    ASSERT_EQ(client.sendData((const unsigned char *) "\x02\x05\x13\x14\x02\x05\x13\x14\x02\x05\x13\x14\x02\x05\x13\x14\x02\x05\x13\x14\x02\x05\x13\x14\x02\x05\x13\x14\x03\x02\x05\x13\x14\x02\x05\x13\x14\x00\x12\x12\x13\x14\x03", 43), 0);
+    for (int i = 0; i < 8; i++){
+        ASSERT_EQ(client.receiveFramedData(), 2);
+        ASSERT_EQ(client.getDataSize(), 1);
+        ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 1);
+        ASSERT_EQ(buffer[0], 0x02);
+        ASSERT_EQ(client.getBuffer(tmp), 1);
+        ASSERT_EQ(tmp.size(), 1);
+        ASSERT_EQ(tmp[0], 0x02);
+    }
+    ASSERT_EQ(client.receiveFramedData(), 0);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 75, true);
+    ASSERT_EQ(client.getDataSize(), 10);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 10);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "\x02\x05\x13\x14\x00\x12\x12\x13\x14\x03", 10), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 10);
+    ASSERT_EQ(tmp.size(), 10);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "\x02\x05\x13\x14\x00\x12\x12\x13\x14\x03", 10), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 0);
+    ASSERT_EQ(tmp.size(), 0);
+}
