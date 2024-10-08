d0e0d25051e7aa0a577e1a9337c39d586914c716
Jaya Wikrama <jayawikrama89@gmail.com>
2024-10-08 18:17:56 +0700

    development/progress: add test for Framed Data Reception Method - 10.

--- test/test-framed-data.cpp
+++ test/test-framed-data.cpp
@@ -715,3 +715,42 @@ TEST_F(TCPFramedDataTest, ReadNegativeTest_1) {
     ASSERT_EQ(tmp.size(), 8);
     ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "567890-=", 8), 0);
 }
+
+TEST_F(TCPFramedDataTest, ReadNegativeTest_2) {
+    unsigned char buffer[16];
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    std::vector <unsigned char> tmp;
+    client.setPort(4431);
+    client.setTimeout(250);
+    client.setKeepAliveMs(1000);
+    DataFrame startBytes(DataFrame::FRAME_TYPE_START_BYTES, "1234");
+    DataFrame cmdBytes(DataFrame::FRAME_TYPE_COMMAND, 1);
+    DataFrame dataBytes(DataFrame::FRAME_TYPE_DATA, 3);
+    DataFrame stopBytes(DataFrame::FRAME_TYPE_STOP_BYTES);
+    client = startBytes + cmdBytes + dataBytes + stopBytes;
+    ASSERT_EQ(client.getFormat()->getDataFrameFormat(),
+              "FRAME_TYPE_START_BYTES[size:4]:<<31323334>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_COMMAND[size:1]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_DATA[size:3]:<<>><<exeFunc:0>><<postFunc:0>>\n"
+              "FRAME_TYPE_STOP_BYTES[size:0]:<<>><<exeFunc:0>><<postFunc:0>>\n");
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.init(), 0);
+    ASSERT_EQ(client.sendData("1234567890-="), 0);
+    ASSERT_EQ(client.receiveFramedData(), 4);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 75, true);
+    ASSERT_EQ(client.getDataSize(), 8);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 8);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "12345678", 8), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 8);
+    ASSERT_EQ(tmp.size(), 8);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "12345678", 8), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 4);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 4);
+    ASSERT_EQ(memcmp(buffer, (const unsigned char *) "90-=", 4), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 4);
+    ASSERT_EQ(tmp.size(), 4);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) "90-=", 4), 0);
+}
