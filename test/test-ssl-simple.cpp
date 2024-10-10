01e2327571eac027b525ee89ede88cbbefe7bf3f
Jaya Wikrama <jayawikrama89@gmail.com>
2024-10-10 07:34:14 +0700

    development/progress: add test for SSL delayed data.

--- test/test-ssl-simple.cpp
+++ test/test-ssl-simple.cpp
@@ -247,3 +247,41 @@ TEST_F(SSLSimpleTest, communicationTest_4) {
     tmp = client.getRemainingBufferAsVector();
     ASSERT_EQ(tmp.size(), 0);
 }
+
+TEST_F(SSLSimpleTest, communicationTest_with_delayed_bytes) {
+    unsigned char buffer[64];
+    std::string sdata = std::string(TEST_STR_1) + std::string(TEST_STR_1) + std::string(TEST_STR_1) + std::string(TEST_STR_1) + std::string(TEST_STR_1);
+    std::vector <unsigned char> tmp;
+    server.setReceptionHandler(&receptionCallbackFunctionEchoDelay, (void *) (long) 50);
+    ASSERT_EQ(client.setPort(4431), true);
+    ASSERT_EQ(client.init(), 0);
+    ASSERT_EQ(client.setKeepAliveMs(75), true);
+    ASSERT_EQ(client.sendData(sdata), 0);
+    for (int i = 0; i < 5; i++){
+        ASSERT_EQ(client.receiveData(13), 0);
+        ASSERT_EQ(client.getDataSize(), 13);
+        ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 13);
+        ASSERT_EQ(memcmp(buffer, (const unsigned char *) sdata.c_str(), 13), 0);
+        ASSERT_EQ(client.getBuffer(tmp), 13);
+        ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) sdata.c_str(), 13), 0);
+        tmp = client.getBufferAsVector();
+        ASSERT_EQ(tmp.size(), 13);
+        ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) sdata.c_str(), 13), 0);
+        usleep(125000);
+    }
+    ASSERT_EQ(client.sendData(sdata), 0);
+    ASSERT_EQ(client.receiveData(), 2);
+    client.closeSocket();
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(buffer[0], 0x00);
+    ASSERT_EQ(client.getBuffer(tmp), 0);
+    tmp = client.getBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 0);
+    ASSERT_EQ(tmp.size(), 0);
+    tmp.clear();
+    tmp = client.getRemainingBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+}
