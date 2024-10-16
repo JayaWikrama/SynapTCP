bede9a42ef531a72c4f2ab834e58dd8e71a476a5
Jaya Wikrama <jayawikrama89@gmail.com>
2024-10-16 20:19:59 +0700

    development/progress: add test for SSL custom reception: negative test.

--- test/test-ssl-simple.cpp
+++ test/test-ssl-simple.cpp
@@ -689,3 +689,120 @@ TEST_F(SSLSimpleTest, communicationTest_rcvNBytes) {
     ASSERT_EQ(tmp.size(), 77);
     ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) (TEST_STR_3 + 385), 77), 0);
 }
+
+TEST_F(SSLSimpleTest, negativeCommunicationTest_not_connected) {
+    unsigned char buffer[8];
+    pthread_t thread;
+    std::vector <unsigned char> tmp;
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    ASSERT_EQ(client.setPort(4431), true);
+    ASSERT_EQ(client.setKeepAliveMs(50), true);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.sendData((const unsigned char *) TEST_STR_3, 462), 1);
+    ASSERT_EQ(client.receiveData(385), 1);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 10, true);
+    ASSERT_EQ(client.getDataSize(), 0);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 0);
+    tmp.clear();
+    tmp = client.getBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    tmp.clear();
+    tmp = client.getRemainingBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+}
+
+TEST_F(SSLSimpleTest, negativeCommunicationTest_try_to_connecting_invalid_url) {
+    unsigned char buffer[8];
+    pthread_t thread;
+    std::vector <unsigned char> tmp;
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    ASSERT_EQ(client.setAddress("1.2.3.4"), true);
+    ASSERT_EQ(client.setPort(4431), true);
+    ASSERT_EQ(client.setKeepAliveMs(50), true);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.sendData((const unsigned char *) TEST_STR_3, 462), 1);
+    ASSERT_EQ(client.receiveData(385), 1);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 10, true);
+    ASSERT_EQ(client.getDataSize(), 0);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 0);
+    tmp.clear();
+    tmp = client.getBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    tmp.clear();
+    tmp = client.getRemainingBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+}
+
+TEST_F(SSLSimpleTest, negativeCommunicationTest_no_input_bytes_available) {
+    unsigned char buffer[8];
+    pthread_t thread;
+    std::vector <unsigned char> tmp;
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    ASSERT_EQ(client.setPort(4431), true);
+    ASSERT_EQ(client.setKeepAliveMs(50), true);
+    ASSERT_EQ(client.setTimeout(250), true);
+    ASSERT_EQ(client.init(), 0);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.receiveData(1), 2);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 245 && diffTime <= 325, true);
+    ASSERT_EQ(client.getDataSize(), 0);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 0);
+    tmp.clear();
+    tmp = client.getBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    tmp.clear();
+    tmp = client.getRemainingBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+}
+
+TEST_F(SSLSimpleTest, negativeCommunicationTest_rcvStopBytes) {
+    unsigned char buffer[16];
+    pthread_t thread;
+    std::vector <unsigned char> tmp;
+    struct timeval tvStart, tvEnd;
+    int diffTime = 0;
+    ASSERT_EQ(client.setPort(4431), true);
+    ASSERT_EQ(client.setKeepAliveMs(50), true);
+    gettimeofday(&tvStart, NULL);
+    ASSERT_EQ(client.init(), 0);
+    ASSERT_EQ(client.sendData((const unsigned char *) TEST_STR_4, 16), 0);
+    ASSERT_EQ(client.receiveStopBytes((const unsigned char *) TEST_STR_4 + 1, 4), 3);
+    gettimeofday(&tvEnd, NULL);
+    diffTime = (tvEnd.tv_sec - tvStart.tv_sec) * 1000 + (tvEnd.tv_usec - tvStart.tv_usec) / 1000;
+    ASSERT_EQ(diffTime >= 0 && diffTime <= 50, true);
+    ASSERT_EQ(client.getDataSize(), 16);
+    ASSERT_EQ(client.getBuffer(buffer, sizeof(buffer)), 16);
+    ASSERT_NE(memcmp(buffer, (const unsigned char *) TEST_STR_4 + 1, 4), 0);
+    ASSERT_EQ(client.getBuffer(tmp), 16);
+    ASSERT_EQ(tmp.size(), 16);
+    ASSERT_NE(memcmp(tmp.data(), (const unsigned char *) TEST_STR_4 + 1, 4), 0);
+    tmp.clear();
+    tmp = client.getBufferAsVector();
+    ASSERT_EQ(tmp.size(), 16);
+    ASSERT_EQ(memcmp(tmp.data(), (const unsigned char *) TEST_STR_4, 4), 0);
+    ASSERT_EQ(client.getRemainingDataSize(), 0);
+    ASSERT_EQ(client.getRemainingBuffer(buffer, sizeof(buffer)), 0);
+    ASSERT_EQ(client.getRemainingBuffer(tmp), 0);
+    ASSERT_EQ(tmp.size(), 0);
+    tmp.clear();
+    tmp = client.getRemainingBufferAsVector();
+    ASSERT_EQ(tmp.size(), 0);
+}
